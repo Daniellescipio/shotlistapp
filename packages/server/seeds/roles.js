0c55668db64a0d2f461d2f550b5813fa40853b70
Danny Holland <holland.danny@gmail.com>
2021-02-17 10:20:16 -0500

    Adds function to repeat a function, Adds people seeding file, Adds reset cmd to server package.json (#21)
    * Adds function to repeate a function, Adds people seeding file
    
    * Adds function to repeat a function, Adds people seeding ability

---
+++ packages/server/seeds/roles.js
@@ -0,0 +1,12 @@
+// Roles Used in the people generator function
+
+module.exports = [
+  "Producer",
+  "Camera",
+  "Director",
+  "Talent",
+  "Grip",
+  "Photographer",
+  "Stylist",
+  "Production Assistant"
+]
--- packages/web/src/components/App.js
+++ packages/web/src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <></>;
+  return <><div>Hey</div></>;
 };
 
 export default App;
