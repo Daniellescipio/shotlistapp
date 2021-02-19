2e9e1571f1866332d179e44f77dd7e85cfe0b65f
steverodri91 <steve.rodri91@gmail.com>
2021-02-18 20:42:35 -0500

    move repeater function to server/helpers/

--- packages/server/helpers/generateSeedData.js
+++ packages/server/helpers/generateSeedData.js
@@ -2,8 +2,6 @@
 // @param generatorName - name of generator function to run
 // @returns an array of objects
 
-const repeater = (number, generatorName) => {
-  return Array.from({ length: number }, () => generatorName())
-}
-
-module.exports = repeater
+module.exports = (number, generatorName) => {
+  return Array.from({ length: number }, () => generatorName());
+};
