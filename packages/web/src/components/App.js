0b6c1dff45c447cc245f8c8608175d9d7770bec7
Steve Rodriguez <steve.rodri91@gmail.com>
2021-02-20 15:06:14 -0500

    Pages (#34)
    * initialize react router
    
    * add Home page
    
    * add ProductionList page
    
    * create Page component
    
    * render Page in App
    
    * move Homepage to pages

--- packages/web/src/components/App.js
+++ packages/web/src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Page from "../components/Page";
 import "../styles/App.css";
 
 const App = () => {
   return (
     <>
-      <div>Hey</div>
+      <Page />
     </>
   );
 };
