fce16f092ced3bb237924ce888a0b018327e5d2c
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 18:38:51 -0500

    fetch random people, equipment, and shots for production seed

--- packages/server/database/seeds/production.js
+++ packages/server/database/seeds/production.js
@@ -1,11 +1,22 @@
 // Use Faker https://github.com/Marak/faker.js for random data
 const faker = require("faker");
 const seeder = require("../seeder");
-const { Production } = require("../../models");
+const { Production, Person, Equipment, Shot } = require("../../models");
 
-const generateProduction = () => ({
+const randomNum = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const fetchSample = async (Model, range) => {
+  const size = randomNum(...range);
+  const collection = Model.aggregate().sample(size).exec();
+  return collection.map(doc => doc._id);
+};
+
+const generateProduction = async () => ({
   name: `${faker.company.companyName()} - ${faker.commerce.productName()}`,
   brief: faker.lorem.paragraph(),
+  people: await fetchSample(Person, { min: 3, max: 15 }),
+  equipment: await fetchSample(Equipment, { min: 5, max: 10 }),
+  shots: await fetchSample(Shot, { min: 10, max: 25 }),
 });
 
 module.exports = async amount =>
