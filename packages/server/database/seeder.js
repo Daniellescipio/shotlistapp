47220e4a569d79a13a3ab05f6eb9fab73e71879d
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 16:47:30 -0500

    add seeder function

---
+++ packages/server/database/seeder.js
@@ -0,0 +1,14 @@
+const { connection } = require("mongoose");
+
+module.exports = async ({ model: Model, plural, generateDoc, amount }) => {
+  // eslint-disable-next-line no-console
+  console.log(`Seeding ${amount} ${plural} into ${connection.name}`);
+
+  const docs = await Promise.all(
+    Array.from({ length: amount }).map(async () => generateDoc())
+  );
+
+  const collection = Model.insertMany(docs);
+  // eslint-disable-next-line no-console
+  console.log(`${plural}: ${collection}`);
+};
