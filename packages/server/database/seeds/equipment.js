419719293d9da4bddfd9ff4031c052df1ba1c038
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 17:36:50 -0500

    add equipment seed template

---
+++ packages/server/database/seeds/equipment.js
@@ -0,0 +1,16 @@
+// Use Faker https://github.com/Marak/faker.js for random data
+const faker = require("faker");
+const seeder = require("../seeder");
+const { Equipment } = require("../../models");
+
+const generateEquipment = () => {
+  return {};
+};
+
+module.exports = async amount =>
+  seeder({
+    model: Equipment,
+    plural: "Equipment",
+    generateDoc: generateEquipment,
+    amount: amount || 20,
+  });
