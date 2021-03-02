ba18b49a29ce5635c78d9f466cbeeee298af55ec
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-02 18:50:26 -0500

    add seeds/index file

---
+++ packages/server/database/seeds/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  seedEquipment: require("./equipment"),
+  seedPeople: require("./person"),
+  seedProductions: require("./production"),
+  seedScenes: require("./scene"),
+  seedShots: require("./shot"),
+};
