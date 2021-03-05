f15bdddccae4a3d8247154cb249a4e4c613bdf35
Danny Holland <holland.danny@gmail.com>
2021-03-05 17:34:02 -0500

    Add Chakra CRA

---
+++ packages/chakra/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <Text fontSize="4xl">Shot List Pro</Text>
+          <VStack spacing={8}>//Add Productions Her</VStack>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
