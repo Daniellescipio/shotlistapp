0293b13fffa8aab749ee42671316a3d51121b35a
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2021-03-11 15:56:36 -0500

    add Chakra Provider, add custom Button variants

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./components/App";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      primary: "#FCC201",
+      secondary: "#B78628",
+    },
+  },
+  config: { initialColorMode: "dark" },
+  components: {
+    Button: {
+      variants: {
+        "primary-with-icon": {
+          bgGradient: "linear(to-b, brand.secondary, brand.primary)",
+          color: "black",
+        },
+        "outline-highlight": {
+          textTransform: "uppercase",
+          borderWidth: 3,
+          borderRadius: 0,
+          borderColor: "transparent",
+          _hover: {
+            borderColor: "brand.primary",
+          },
+        },
+      },
+    },
+  },
+});
 
 const Root = () => (
   <Router>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </Router>
 );
 
